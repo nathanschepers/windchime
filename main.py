8a0f26cbdf11705eda79efe0f401166904cdbb95
Nathan <nathan.schepers@gmail.com>
2021-03-16 10:45:08 +0000

    added chime_loop metronome

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import random
-from collections import Counter
+from time import perf_counter, sleep
 
 wind_forces = [*range(0, 5)]
 wind_probabilities = [0.15, 0.15, 0.40, 0.15, 0.15]
 
 
-def output_stuff():
-    print(f'forces - {wind_forces}, probabilities - {wind_probabilities}')
-    gusts = []
-    for _ in range(1000):
-        gusts.extend(random.choices(wind_forces, wind_probabilities))
-
-    print(f'all gusts - {gusts}')
-    print(f'all gusts (counted) - {Counter(gusts)}')
+def get_gust():
+    """return a new random wind gust based on wind_probabilities distribution"""
+    return random.choices(wind_forces, wind_probabilities)[0]
 
 
-# Press the green button in the gutter to run the script.
+def play_chime():
+    """play the chime"""
+    print(f'{get_gust()}')
+
+
+def chime_loop(bpm=60):
+    """the overall model/metronome loop"""
+    delay = d = 60 / bpm
+    prev = perf_counter()
+    for _ in range(200):
+        sleep(d)
+        t = perf_counter()
+        delta = t - prev - delay
+        print(f'tick drift - {delta:+.9f}')
+        play_chime()
+        d -= delta
+        prev = t
+
+
 if __name__ == '__main__':
-    output_stuff()
+    chime_loop(bpm=200)
